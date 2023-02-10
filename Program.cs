9bf402ebb183de6b5fc0fa439e13fe3e0775f6ea
szheng8 <szheng8@crimson.ua.edu>
2023-02-10 15:22:56 -0600

    Lab4

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿// start main
+int option = -1;
+
+while(option != 3){
+    DisplayMenu();
+    if(IsValidOption(ref option)){
+        Triangle(option);
+    }
+}
+return;
+// end main
+
+static void DisplayMenu(){
+    System.Console.WriteLine("\nEnter 1 to display full triangle\nEnter 2 to display partial triangle\nEnter 3 to exit");
+}
+
+static bool IsValidOption(ref int option){
+    string userInput = (Console.ReadLine());
+    if (userInput == "1" || userInput == "2" || userInput == "3"){
+        option = int.Parse(userInput);
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+static void Triangle(int option){
+    if(option == 1){
+        GetFull();
+    }
+    else if(option == 2){
+        GetPartial();
+    }
+    else return;
+}
+
+static void GetFull(){
+    Random rnd = new Random();
+    int row = rnd.Next(3,9);
+    System.Console.WriteLine("Number of rows: " + row);
+
+    for (int i = 0; i <= row; i++){
+        for (int j = 0; j < i; j++){
+            System.Console.Write("O ");
+        }
+        System.Console.Write("\n");
+    }
+}
+
+static void GetPartial(){
+    Random rnd = new Random();
+    int row = rnd.Next(3,9);
+    System.Console.WriteLine("Number of rows: "+ row);
+
+    for (int i = 0; i <= row; i++){
+        Random rndm = new Random();
+        int num = rndm.Next(2);
+
+        for (int j = 0; j < i; j++){
+            if(num == 1){
+                System.Console.Write("O ");
+                num = rndm.Next(2);
+            }
+            else{
+                System.Console.Write("  ");
+                num = rndm.Next(2);
+            }
+        }
+        System.Console.Write("\n");
+    }
+}
